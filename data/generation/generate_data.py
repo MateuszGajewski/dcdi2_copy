2995a82fd99a835e21f93b21c7ec5f8afd715024
MateuszGajewski1 <mg96272@gmail.com>
2024-02-12 10:06:04 +0000

    Dumping json file with dataset parameters

--- data/generation/generate_data.py
+++ data/generation/generate_data.py
@@ -1,19 +1,40 @@
-import os
 import argparse
 import csv
+import json
+import os
+
 import numpy as np
-import dag_generator as gen
 from sklearn.preprocessing import StandardScaler
+
+import dag_generator as gen
 
 
 class dataset_generator:
-    """ Generate datasets using dag_generator.py. `nb_dag` dags are sampled and
+    """Generate datasets using dag_generator.py. `nb_dag` dags are sampled and
     then data are generated accordingly to the chosen parameters (e.g.
-    mechanisms). Can generate dataset with 'hard stochastic' interventions """
+    mechanisms). Can generate dataset with 'hard stochastic' interventions"""
 
-    def __init__(self, mechanism, cause, intervention_type, struct_interv_distr, noise, noise_coeff,
-                 nb_nodes, expected_degree, nb_points, suffix, rescale, obs_data=True, nb_interventions=10,
-                 min_nb_target=1, max_nb_target=3, conservative=False, cover=False, verbose=True):
+    def __init__(
+        self,
+        mechanism,
+        cause,
+        intervention_type,
+        struct_interv_distr,
+        noise,
+        noise_coeff,
+        nb_nodes,
+        expected_degree,
+        nb_points,
+        suffix,
+        rescale,
+        obs_data=True,
+        nb_interventions=10,
+        min_nb_target=1,
+        max_nb_target=3,
+        conservative=False,
+        cover=False,
+        verbose=True,
+    ):
         """
         Generate a dataset containing interventions. The setting is similar to the
         one in the GIES paper (Characterization and greedy learning of interventional Markov
@@ -60,7 +81,9 @@ class dataset_generator:
         self.nb_points = nb_points
         self.suffix = suffix
         self.rescale = rescale
-        self.folder = f'data_p{nb_nodes}_e{nb_nodes * expected_degree}_n{nb_points}_{suffix}'
+        self.folder = (
+            f"data_p{nb_nodes}_e{nb_nodes * expected_degree}_n{nb_points}_{suffix}"
+        )
         self.verbose = verbose
         self.generator = None
         self.struct_interv_distr = struct_interv_distr
@@ -79,34 +102,71 @@ class dataset_generator:
         self._create_folder()
 
     def _checkup(self):
-        possible_mechanisms = ["linear","polynomial","sigmoid_add","sigmoid_mix","gp_add","gp_mix",
-                               "anm","nn","nn_add","pnl_gp_mechanism","pnl_mult_mechanism","post_nonlinear","x","circle","adn"]
-        possible_causes = ["gmm_cause","gaussian","variable_gaussian","uniform","uniform_positive"]
-        possible_noises = ["gaussian","variable_gaussian","uniform","laplace","absolute_gaussian","nn"]
+        possible_mechanisms = [
+            "linear",
+            "polynomial",
+            "sigmoid_add",
+            "sigmoid_mix",
+            "gp_add",
+            "gp_mix",
+            "anm",
+            "nn",
+            "nn_add",
+            "pnl_gp_mechanism",
+            "pnl_mult_mechanism",
+            "post_nonlinear",
+            "x",
+            "circle",
+            "adn",
+        ]
+        possible_causes = [
+            "gmm_cause",
+            "gaussian",
+            "variable_gaussian",
+            "uniform",
+            "uniform_positive",
+        ]
+        possible_noises = [
+            "gaussian",
+            "variable_gaussian",
+            "uniform",
+            "laplace",
+            "absolute_gaussian",
+            "nn",
+        ]
 
-        assert self.mechanism in possible_mechanisms, \
-                f"mechanism doesn't exist. It has to be in {possible_mechanisms}"
-        assert self.cause in possible_causes, \
-                f"initial cause doesn't exist. It has to be in {possible_causes}"
-        assert self.noise in possible_noises, \
-                f"noise doesn't exist. It has to be in {possible_noises}"
+        assert (
+            self.mechanism in possible_mechanisms
+        ), f"mechanism doesn't exist. It has to be in {possible_mechanisms}"
+        assert (
+            self.cause in possible_causes
+        ), f"initial cause doesn't exist. It has to be in {possible_causes}"
+        assert (
+            self.noise in possible_noises
+        ), f"noise doesn't exist. It has to be in {possible_noises}"
 
-        assert self.nb_interventions <= self.nb_points, \
-                "nb_interventions should be smaller or equal to nb_points"
-        assert self.min_nb_target <= self.max_nb_target, \
-                "min_nb_target should be smaller or equal to max_nb_target"
-        assert self.max_nb_target <= self.nb_nodes, \
-                "max_nb_target should be smaller or equal to the total number of nodes (nb_nodes)"
+        assert (
+            self.nb_interventions <= self.nb_points
+        ), "nb_interventions should be smaller or equal to nb_points"
+        assert (
+            self.min_nb_target <= self.max_nb_target
+        ), "min_nb_target should be smaller or equal to max_nb_target"
+        assert (
+            self.max_nb_target <= self.nb_nodes
+        ), "max_nb_target should be smaller or equal to the total number of nodes (nb_nodes)"
         if self.cover:
-            assert self.max_nb_target * self.nb_interventions >= self.nb_nodes, \
-                    "In order to cover, there should be more interventions or a higher min_nb_target"
+            assert (
+                self.max_nb_target * self.nb_interventions >= self.nb_nodes
+            ), "In order to cover, there should be more interventions or a higher min_nb_target"
         if self.conservative and not self.obs_data:
-            assert (self.nb_nodes - self.max_nb_target) * self.nb_interventions >= self.nb_nodes
+            assert (
+                self.nb_nodes - self.max_nb_target
+            ) * self.nb_interventions >= self.nb_nodes
 
     def _create_folder(self):
         """Create folders
 
-        fname(str): path """
+        fname(str): path"""
         try:
             os.mkdir(self.folder)
         except OSError:
@@ -114,49 +174,63 @@ class dataset_generator:
 
     def _initialize_dag(self):
         if self.verbose:
-            print(f'Sampling the DAG #{self.i_dataset}')
-        self.generator = gen.DagGenerator(self.mechanism,
-                                         noise=self.noise,
-                                         noise_coeff=self.noise_coeff,
-                                         cause=self.cause,
-                                         npoints=self.nb_points,
-                                         nodes=self.nb_nodes,
-                                         expected_density=self.expected_degree,
-                                         rescale=self.rescale)
+            print(f"Sampling the DAG #{self.i_dataset}")
+        self.generator = gen.DagGenerator(
+            self.mechanism,
+            noise=self.noise,
+            noise_coeff=self.noise_coeff,
+            cause=self.cause,
+            npoints=self.nb_points,
+            nodes=self.nb_nodes,
+            expected_density=self.expected_degree,
+            rescale=self.rescale,
+        )
 
         self.generator.init_variables()
-        self.generator.save_dag_cpdag(self.folder, i+1)
+        self.generator.save_dag_cpdag(self.folder, i + 1)
 
     def _pick_targets(self, nb_max_iteration=100000):
         nodes = np.arange(self.nb_nodes)
         not_correct = True
         i = 0
 
-        if(self.max_nb_target == 1):
-            intervention = np.random.choice(self.nb_nodes, self.nb_interventions, replace=False)
+        if self.max_nb_target == 1:
+            intervention = np.random.choice(
+                self.nb_nodes, self.nb_interventions, replace=False
+            )
             targets = [[i] for i in intervention]
 
         else:
-            while(not_correct and i < nb_max_iteration):
+            while not_correct and i < nb_max_iteration:
                 targets = []
                 not_correct = False
                 i += 1
 
                 # pick targets randomly
                 for _ in range(self.nb_interventions):
-                    nb_targets = np.random.randint(self.min_nb_target, self.max_nb_target+1, 1)
-                    intervention = np.random.choice(self.nb_nodes, nb_targets, replace=False)
+                    nb_targets = np.random.randint(
+                        self.min_nb_target, self.max_nb_target + 1, 1
+                    )
+                    intervention = np.random.choice(
+                        self.nb_nodes, nb_targets, replace=False
+                    )
                     targets.append(intervention)
 
                 # apply rejection sampling
                 if self.cover and not self._is_covering(nodes, targets):
                     not_correct = True
-                if self.conservative and not self.obs_data and not self._is_conservative(nodes, targets):
+                if (
+                    self.conservative
+                    and not self.obs_data
+                    and not self._is_conservative(nodes, targets)
+                ):
                     not_correct = True
 
             if i == nb_max_iteration:
-                raise ValueError("Could generate appropriate targets. \
-                                 Exceeded the maximal number of iterations")
+                raise ValueError(
+                    "Could generate appropriate targets. \
+                                 Exceeded the maximal number of iterations"
+                )
 
             for i, t in enumerate(targets):
                 targets[i] = np.sort(t)
@@ -187,20 +261,20 @@ class dataset_generator:
 
     def _save_data(self, i, data, regimes=None, mask=None):
         if mask is None:
-            data_path = os.path.join(self.folder, f'data{i+1}.npy')
+            data_path = os.path.join(self.folder, f"data{i+1}.npy")
             np.save(data_path, data)
         else:
-            data_path = os.path.join(self.folder, f'data_interv{i+1}.npy')
+            data_path = os.path.join(self.folder, f"data_interv{i+1}.npy")
             np.save(data_path, data)
 
-            data_path = os.path.join(self.folder, f'intervention{i+1}.csv')
-            with open(data_path, 'w', newline="") as f:
+            data_path = os.path.join(self.folder, f"intervention{i+1}.csv")
+            with open(data_path, "w", newline="") as f:
                 writer = csv.writer(f)
                 writer.writerows(mask)
 
         if regimes is not None:
-            regime_path = os.path.join(self.folder, f'regime{i+1}.csv')
-            with open(regime_path, 'w', newline="") as f:
+            regime_path = os.path.join(self.folder, f"regime{i+1}.csv")
+            with open(regime_path, "w", newline="") as f:
                 writer = csv.writer(f)
                 for regime in regimes:
                     writer.writerow([regime])
@@ -218,7 +292,9 @@ class dataset_generator:
             else:
                 div = self.nb_interventions
             # one-liner taken from https://stackoverflow.com/questions/20348717/algo-for-dividing-a-number-into-almost-equal-whole-numbers/20348992
-            points_per_interv = [num // div + (1 if x < num % div else 0)  for x in range (div)]
+            points_per_interv = [
+                num // div + (1 if x < num % div else 0) for x in range(div)
+            ]
             # points_per_interv = int(self.nb_points / self.nb_interventions)
             mask_intervention = []
             regimes = []
@@ -232,10 +308,12 @@ class dataset_generator:
                 targets = target_list[j]
 
                 # generate the datasets with the given interventions
-                dataset, _ = self.generator.intervene(self.intervention_type,
-                                                      targets,
-                                                      points_per_interv[j],
-                                                      self.struct_interv_distr)
+                dataset, _ = self.generator.intervene(
+                    self.intervention_type,
+                    targets,
+                    points_per_interv[j],
+                    self.struct_interv_distr,
+                )
                 self.generator.reinitialize()
 
                 # put dataset and targets in arrays
@@ -246,7 +324,7 @@ class dataset_generator:
                 end = start + points_per_interv[j]
                 data[start:end, :] = dataset
                 mask_intervention.extend([targets for i in range(points_per_interv[j])])
-                regimes.extend([j+1 for i in range(points_per_interv[j])])
+                regimes.extend([j + 1 for i in range(points_per_interv[j])])
 
             # setting without interventions
             if self.obs_data:
@@ -298,54 +376,120 @@ def rotate(point, angle):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--mechanism', type=str, default='anm',
-                        help='Type of mechanism use to generate the data ')
-    parser.add_argument('--intervention-type', type=str, default='structural',
-                        help='Type of intervention (structural or parametric)')
-    parser.add_argument('--initial-cause', type=str, default='uniform',
-                        help='Distribution of initial causes')
-    parser.add_argument('--struct-interv-distr', type=str, default='uniform',
-                        help='Distribution of intervened node')
-    parser.add_argument('--noise', type=str, default='variable_gaussian',
-                        help='Distribution of noises')
-    parser.add_argument('--noise-coeff', type=float, default=0.4,
-                        help='Noise coefficient')
-    parser.add_argument('--nb-nodes', type=int, default=10,
-                        help='Number of nodes in the DAGs')
-    parser.add_argument('--expected-degree', type=float, default=1,
-                        help='Expected number of edges per node')
-    parser.add_argument('--nb-dag', type=int, default=10,
-                        help='Number of DAGs to generate dataset from')
-    parser.add_argument('--nb-points', type=int, default=1000,
-                        help='Number of points per dataset')
-    parser.add_argument('--rescale', action='store_true',
-                        help='Rescale the variables')
-    parser.add_argument('--suffix', type=str, default='GP',
-                        help='Suffix that will be added at the \
-                        end of the folder name')
+    parser.add_argument(
+        "--mechanism",
+        type=str,
+        default="anm",
+        help="Type of mechanism use to generate the data ",
+    )
+    parser.add_argument(
+        "--intervention-type",
+        type=str,
+        default="structural",
+        help="Type of intervention (structural or parametric)",
+    )
+    parser.add_argument(
+        "--initial-cause",
+        type=str,
+        default="uniform",
+        help="Distribution of initial causes",
+    )
+    parser.add_argument(
+        "--struct-interv-distr",
+        type=str,
+        default="uniform",
+        help="Distribution of intervened node",
+    )
+    parser.add_argument(
+        "--noise", type=str, default="variable_gaussian", help="Distribution of noises"
+    )
+    parser.add_argument(
+        "--noise-coeff", type=float, default=0.4, help="Noise coefficient"
+    )
+    parser.add_argument(
+        "--nb-nodes", type=int, default=10, help="Number of nodes in the DAGs"
+    )
+    parser.add_argument(
+        "--expected-degree",
+        type=float,
+        default=1,
+        help="Expected number of edges per node",
+    )
+    parser.add_argument(
+        "--nb-dag", type=int, default=10, help="Number of DAGs to generate dataset from"
+    )
+    parser.add_argument(
+        "--nb-points", type=int, default=1000, help="Number of points per dataset"
+    )
+    parser.add_argument("--rescale", action="store_true", help="Rescale the variables")
+    parser.add_argument(
+        "--suffix",
+        type=str,
+        default="GP",
+        help="Suffix that will be added at the \
+                        end of the folder name",
+    )
 
     # Arguments related to interventions
-    parser.add_argument('--intervention', action='store_true',
-                        help='if True, generate data with interventions')
-    parser.add_argument('--nb-interventions', type=int, default=3,
-                        help='number of interventional settings')
-    parser.add_argument('--obs-data', action='store_true',
-                        help='if True, the first setting is generated without any interventions')
-    parser.add_argument('--min-nb-target', type=int, default=1,
-                        help='minimal number of targets per setting')
-    parser.add_argument('--max-nb-target', type=int, default=3,
-                        help='maximal number of targets per setting')
-    parser.add_argument('--conservative', action='store_true',
-                        help='if True, make sure that the intervention family is conservative: i.e. that all nodes have not been intervened in at least one setting.')
-    parser.add_argument('--cover', action='store_true',
-                        help='if True, make sure that all nodes have been intervened on at least in one setting.')
+    parser.add_argument(
+        "--intervention",
+        action="store_true",
+        help="if True, generate data with interventions",
+    )
+    parser.add_argument(
+        "--nb-interventions",
+        type=int,
+        default=3,
+        help="number of interventional settings",
+    )
+    parser.add_argument(
+        "--obs-data",
+        action="store_true",
+        help="if True, the first setting is generated without any interventions",
+    )
+    parser.add_argument(
+        "--min-nb-target",
+        type=int,
+        default=1,
+        help="minimal number of targets per setting",
+    )
+    parser.add_argument(
+        "--max-nb-target",
+        type=int,
+        default=3,
+        help="maximal number of targets per setting",
+    )
+    parser.add_argument(
+        "--conservative",
+        action="store_true",
+        help="if True, make sure that the intervention family is conservative: i.e. that all nodes have not been intervened in at least one setting.",
+    )
+    parser.add_argument(
+        "--cover",
+        action="store_true",
+        help="if True, make sure that all nodes have been intervened on at least in one setting.",
+    )
 
     arg = parser.parse_args()
-    generator = dataset_generator(arg.mechanism, arg.initial_cause,
-                                  arg.intervention_type, arg.struct_interv_distr, arg.noise, arg.noise_coeff, arg.nb_nodes,
-                                  arg.expected_degree, arg.nb_points, arg.suffix, arg.rescale,
-                                  arg.obs_data, arg.nb_interventions, arg.min_nb_target, arg.max_nb_target,
-                                  arg.conservative, arg.cover)
+    generator = dataset_generator(
+        arg.mechanism,
+        arg.initial_cause,
+        arg.intervention_type,
+        arg.struct_interv_distr,
+        arg.noise,
+        arg.noise_coeff,
+        arg.nb_nodes,
+        arg.expected_degree,
+        arg.nb_points,
+        arg.suffix,
+        arg.rescale,
+        arg.obs_data,
+        arg.nb_interventions,
+        arg.min_nb_target,
+        arg.max_nb_target,
+        arg.conservative,
+        arg.cover,
+    )
 
     for i in range(arg.nb_dag):
         generator.i_dataset = i
@@ -359,3 +503,7 @@ if __name__ == "__main__":
         if arg.intervention:
             print("Generating the interventional data...")
             generator.generate(True)
+
+    args_dict = vars(arg)
+    with open(f"{generator.folder}/dataset_params.txt", "w") as fp:
+        json.dump(args_dict, fp)
